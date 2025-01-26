c811ce82c2bce6a66bbc49111bf2b3d9a72544fd
Alex Trimpe <alexwtrimpe@gmail.com>
2025-01-26 10:45:07 -0500

    Improving mobile accessibility

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { PolishedPineComponent } from '../../icons/polished-pine.component';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
-  imports: [ButtonModule, CommonModule, RouterModule],
+  imports: [ButtonModule, CommonModule, RouterModule, PolishedPineComponent],
   templateUrl: `./home.component.html`,
 })
 export class HomeComponent implements OnInit {
