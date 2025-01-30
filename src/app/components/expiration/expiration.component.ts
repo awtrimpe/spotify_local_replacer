7388130f54c573e77bf0a4ae98f80830ce1f4e66
Alex Trimpe <alexwtrimpe@gmail.com>
2025-01-30 12:47:56 -0500

    Fixing count position for user. Adjust expiration timer

--- src/app/components/expiration/expiration.component.ts
+++ src/app/components/expiration/expiration.component.ts
@@ -13,7 +13,7 @@ import { AuthService } from '../../services/auth/auth.service';
       <b>Session Expired:</b> Please login again. Your information will not be
       carried over so take note of your current position if you are not
       finished.
-      <p-button routerLink="/login" label="Login" />
+      <p-button routerLink="/login" rounded="true" label="Login" />
     </p-message>
   `,
 })
@@ -21,13 +21,15 @@ export class ExpirationComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   sessionExp = false;
+  timeout?: NodeJS.Timeout;
 
   ngOnInit() {
     const exp_time = this.authService.getExpiration() as Date;
-    const timeout = Math.floor(
-      (exp_time.getTime() - new Date().getTime()) / 1000,
-    );
-    setTimeout(() => {
+    const timeout = Math.floor(exp_time.getTime() - new Date().getTime());
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(() => {
       this.sessionExp = true;
       window.scrollTo(0, 0);
     }, timeout);
