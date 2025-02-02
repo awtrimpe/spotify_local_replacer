7671710d01dbf27fac50c59e3d88d6cdf65670f7
Alex Trimpe <alexwtrimpe@gmail.com>
2025-02-02 14:14:06 -0500

    More unit tests

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -30,4 +30,45 @@ describe('AuthService', () => {
       expect(service.isLoggedIn()).toBeFalse();
     });
   });
+
+  describe('setToken()', () => {
+    it('should set token with passed value', () => {
+      const localSpy = spyOn(window.localStorage, 'setItem');
+      const value = 'abc123';
+      service.setToken(value);
+      expect(localSpy).toHaveBeenCalledWith('token', value);
+    });
+  });
+
+  describe('setExpiration()', () => {
+    it('should set expiration time with current time plus passed seconds', () => {
+      const localSpy = spyOn(window.localStorage, 'setItem');
+      const value = 3600;
+      service.setExpiration(value);
+      const date = new Date();
+      date.setSeconds(date.getSeconds() + value);
+      expect(localSpy).toHaveBeenCalledWith('exp', date.toString());
+    });
+  });
+
+  describe('isExpired()', () => {
+    it('should return true if the expiration is in the past', () => {
+      const t = new Date();
+      t.setSeconds(t.getSeconds() - 1000);
+      spyOn(window.localStorage, 'getItem').and.returnValue(t.toString());
+      expect(service.isExpired()).toBeTrue();
+    });
+
+    it('should return false if the expiration is in the future', () => {
+      const t = new Date();
+      t.setSeconds(t.getSeconds() + 1000);
+      spyOn(window.localStorage, 'getItem').and.returnValue(t.toString());
+      expect(service.isExpired()).toBeFalse();
+    });
+
+    it('should return true if no value set', () => {
+      spyOn(window.localStorage, 'getItem').and.returnValue(null);
+      expect(service.isExpired()).toBeTrue();
+    });
+  });
 });
